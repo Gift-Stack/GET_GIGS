462441ffeb71fc90eeaa3586eec8bd6f0f422c88
Gift-Stack <gkenny896@gmail.com>
2021-10-16 08:11:10 +0100

    Removed pading from appLayout on small screens

--- src/components/app-layout/style.tsx
+++ src/components/app-layout/style.tsx
@@ -1,31 +1,31 @@
-import Grid from '@material-ui/core/Grid'
-import styled from 'styled-components'
+import Grid from "@material-ui/core/Grid";
+import styled from "styled-components";
 
 interface AppLayoutContentWrapperProps {
-  startPage: boolean
-  smallerThan768px: boolean
-  open: boolean
+  startPage: boolean;
+  smallerThan768px: boolean;
+  open: boolean;
 }
 
 export const AppLayoutWrapper = styled(Grid)`
   min-height: 100vh;
   display: flex;
   background-color: var(--white);
-`
+`;
 
 export const AppLayoutContentWrapper = styled(Grid)`
   display: flex;
   padding: ${(props: AppLayoutContentWrapperProps) =>
-    props.smallerThan768px ? '18px' : '18px 32px'};
+    props.smallerThan768px ? "18px 2px" : "18px 32px"};
   border-top-left-radius: 25px;
   border-bottom-left-radius: 25px;
   background: ${(props: AppLayoutContentWrapperProps) =>
     props.startPage && !props.smallerThan768px
-      ? 'var(--white)'
-      : 'var(--primary-background)'};
+      ? "var(--white)"
+      : "var(--primary-background)"};
   width: 100%;
   flex-direction: column;
   overflow-x: hidden;
   opacity: ${(props: AppLayoutContentWrapperProps) =>
-    props.smallerThan768px ? props.open && '0.3' : '1'};
-`
+    props.smallerThan768px ? props.open && "0.3" : "1"};
+`;
